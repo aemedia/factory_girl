c1ce50826dd36be6459d00504dd4040e0f178237
Jonathan Lim <snowblink@gmail.com>
2010-08-10 12:08:53 +0100

    Remove ams callback system in favour of thoughtbot's

--- lib/factory_girl/proxy/create.rb
+++ lib/factory_girl/proxy/create.rb
@@ -2,7 +2,6 @@ class Factory
   class Proxy #:nodoc:
     class Create < Build #:nodoc:
       def result
-        @instance = super
         run_callbacks(:before_create)
         run_callbacks(:after_build)
         @instance.save!
